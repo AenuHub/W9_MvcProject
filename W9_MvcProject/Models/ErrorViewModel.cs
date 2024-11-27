20901776544f2752f8ea9d8839d37275fd3bec2a
AenuHub <jesuskane92@gmail.com>
2024-11-27 11:12:49 +0300

    Initial commit

---
+++ W9_MvcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace W9_MvcProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
