6007b5fbdc9cce0a5840eb92a171ac2927b7642c
AenuHub <jesuskane92@gmail.com>
2024-11-27 15:03:18 +0300

    add GetFullName method

--- W9_MvcProject/Models/Author.cs
+++ W9_MvcProject/Models/Author.cs
@@ -6,4 +6,9 @@ public class Author
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime DateOfBirth { get; set; }
+    
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
 }
