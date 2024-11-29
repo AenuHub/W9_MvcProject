ebfc970f7d9a089306c2517aac67d1c02e2f90eb
AenuHub <jesuskane92@gmail.com>
2024-11-29 13:18:30 +0300

    create BookController.cs

---
+++ W9_MvcProject/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W9_MvcProject.Models;
+
+namespace W9_MvcProject.Controllers;
+
+public class BookController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Add(Book book)
+    {
+        return View();
+    }
+}
