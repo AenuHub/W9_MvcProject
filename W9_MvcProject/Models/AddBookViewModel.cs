e953ffee444e0f7a7393e36e368e399af5adeba6
AenuHub <jesuskane92@gmail.com>
2024-11-29 14:33:48 +0300

    update AddBookViewModel.cs

--- W9_MvcProject/Models/AddBookViewModel.cs
+++ W9_MvcProject/Models/AddBookViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace W9_MvcProject.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W9_MvcProject.Models;
 
 public class AddBookViewModel
-{
+{   
+    [Display(Name = "Book Title")]
+    public string Title { get; set; } = string.Empty;
     
+    [Display(Name = "Author")]
+    public int AuthorId { get; set; }
+    
+    [Display(Name = "Genre")]
+    public string Genre { get; set; } = string.Empty;
+    
+    [Display(Name = "Publish Date")]
+    [DataType(DataType.Date)]
+    public DateTime PublishDate { get; set; }
+    
+    [Display(Name = "ISBN")]
+    public string Isbn { get; set; } = string.Empty;
+    
+    [Display(Name = "Copies Available")]
+    public int CopiesAvailable { get; set; }
 }
