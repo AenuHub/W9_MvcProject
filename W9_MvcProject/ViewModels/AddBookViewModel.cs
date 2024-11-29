c22303847a954c6dfa28581fdde2dc480920454c
AenuHub <jesuskane92@gmail.com>
2024-11-29 15:19:05 +0300

    add range for properties

--- W9_MvcProject/ViewModels/AddBookViewModel.cs
+++ W9_MvcProject/ViewModels/AddBookViewModel.cs
@@ -6,6 +6,7 @@ public class AddBookViewModel
 {   
     [Display(Name = "Book Title")]
     [Required(ErrorMessage = "Title is required")]
+    [MaxLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
     public string Title { get; set; } = string.Empty;
     
     [Display(Name = "Author")]
@@ -19,13 +20,17 @@ public class AddBookViewModel
     [Display(Name = "Publish Date")]
     [DataType(DataType.Date)]
     [Required(ErrorMessage = "Publish Date is required")]
+    [Range(typeof(DateTime), "1/1/1800", "1/1/2100", 
+        ErrorMessage = "Publish Date must be between 01.01.1800 and 01.01.2100")]
     public DateTime? PublishDate { get; set; }
     
     [Display(Name = "ISBN")]
     [Required(ErrorMessage = "ISBN is required")]
+    [Length(17, 17, ErrorMessage = "ISBN must be 17 characters with hyphens")]
     public string Isbn { get; set; } = string.Empty;
     
     [Display(Name = "Copies Available")]
     [Required(ErrorMessage = "The number of available copies is required")]
+    [Range(0, 10, ErrorMessage = "The number of available copies must be between 0 and 10")]
     public int? CopiesAvailable { get; set; }
 }
