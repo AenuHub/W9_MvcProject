28e11ed58cbbe55afcda6ce3176cb92b0258fc3c
AenuHub <jesuskane92@gmail.com>
2024-11-29 14:20:46 +0300

    update Book.cs

--- W9_MvcProject/Models/Book.cs
+++ W9_MvcProject/Models/Book.cs
@@ -1,12 +1,14 @@
-﻿namespace W9_MvcProject.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W9_MvcProject.Models;
 
 public class Book
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     public int AuthorId { get; set; }
-    public string Genre { get; set; }
+    public string Genre { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
-    public string ISBN { get; set; }
+    public string Isbn { get; set; } = string.Empty;
     public int CopiesAvailable { get; set; }
 }
