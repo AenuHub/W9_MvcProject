2887b627697a930a768d780e0550d343c78bc1f8
AenuHub <jesuskane92@gmail.com>
2024-11-29 12:37:14 +0300

    remove files

--- W9_MvcProject/Models/AuthorViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace W9_MvcProject.Models;
-
-public class AuthorViewModel
-{
-    public Author Author { get; set; }
-    public List<Book> Books { get; set; }
-}
--- W9_MvcProject/Models/BookViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace W9_MvcProject.Models;
-
-public class BookViewModel
-{
-    public Book Book { get; set; }
-}
