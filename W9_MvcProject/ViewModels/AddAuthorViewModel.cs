394273fdd638910bb3e118c4cbebe38dc566976c
AenuHub <jesuskane92@gmail.com>
2024-12-04 15:43:16 +0300

    create AddAuthorViewModel.cs

---
+++ W9_MvcProject/ViewModels/AddAuthorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W9_MvcProject.Models;
+
+public class AddAuthorViewModel : Author
+{
+    [Display(Name = "Author First Name")]
+    [Required(ErrorMessage = "First Name is required")]
+    [MaxLength(25, ErrorMessage = "First Name cannot be longer than 25 characters")]
+    public string FirstName { get; set; } = string.Empty;
+    
+    [Display(Name = "Author Last Name")]
+    [Required(ErrorMessage = "Last Name is required")]
+    [MaxLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters")]
+    public string LastName { get; set; } = string.Empty;
+    
+    [Display(Name = "Date of Birth")]
+    [DataType(DataType.Date)]
+    [Required(ErrorMessage = "Date of Birth is required")]
+    [Range(typeof(DateTime), "1/1/1800", "1/1/2010", 
+        ErrorMessage = "Date of Birth must be between 01.01.1800 and 01.01.2010")]
+    public DateTime? DateOfBirth { get; set; }
+}
