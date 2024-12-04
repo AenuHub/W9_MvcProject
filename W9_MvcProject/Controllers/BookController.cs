1c7695bdf93a3f064d4a7958538d2354a856c4b5
AenuHub <jesuskane92@gmail.com>
2024-12-04 13:08:02 +0300

    add edit actions

--- W9_MvcProject/Controllers/BookController.cs
+++ W9_MvcProject/Controllers/BookController.cs
@@ -36,4 +36,50 @@ public class BookController : Controller
         
         return View();
     }
+
+    public IActionResult Edit(int id)
+    {
+        var book = Data.Books.FirstOrDefault(b => b.Id == id);
+        
+        if (book == null)
+        {
+            return NotFound();
+        }
+        
+        var viewModel = new BookDetailsViewModel
+        {
+            Id = book.Id,
+            Title = book.Title,
+            AuthorId = book.AuthorId,
+            Genre = book.Genre,
+            PublishDate = book.PublishDate,
+            Isbn = book.Isbn,
+            CopiesAvailable = book.CopiesAvailable
+        };
+        
+        return View(viewModel);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(BookDetailsViewModel book)
+    {
+        if (ModelState.IsValid)
+        {
+            var existingBook = Data.Books.FirstOrDefault(b => b.Id == book.Id);
+            
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.AuthorId = book.AuthorId.Value;
+                existingBook.Genre = book.Genre;
+                existingBook.PublishDate = book.PublishDate.Value;
+                existingBook.Isbn = book.Isbn;
+                existingBook.CopiesAvailable = book.CopiesAvailable.Value;
+                
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        
+        return View();
+    }
 }
