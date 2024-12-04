54feea6a7ad31575b49faa970d67d128fa2e33b7
AenuHub <jesuskane92@gmail.com>
2024-12-04 15:43:06 +0300

    create AuthorController.cs

---
+++ W9_MvcProject/Controllers/AuthorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W9_MvcProject.Models;
+
+namespace W9_MvcProject.Controllers;
+
+public class AuthorController : Controller
+{
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Add(AddAuthorViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            Data.Authors.Add(new Author
+                {
+                    Id = Data.Authors.Max(a => a.Id) + 1,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                }
+            );
+            return RedirectToAction("Index", "Home");
+        }
+        
+        return View();
+    }
+}
