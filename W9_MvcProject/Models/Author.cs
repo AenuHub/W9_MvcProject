234096d45ca94966f79fd3858ff758f46ae3c80c
AenuHub <jesuskane92@gmail.com>
2024-12-04 11:08:49 +0300

    update Author.cs

--- W9_MvcProject/Models/Author.cs
+++ W9_MvcProject/Models/Author.cs
@@ -3,8 +3,8 @@
 public class Author
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
     
     public string GetFullName()
